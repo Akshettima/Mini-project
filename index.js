90889b60e7c9e54e432197e2ee41ae4c813a6c31
Akshettima <ak.shettimaa@gmail.com>
2025-05-05 16:09:42 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const inputValue=getElementId("input");
+
+const calculate=document
+.querySelectorAll(".functions")
+.forEach(function (item) {
+    item.addEventListener("click", function (e) {});
+});
+    
+const number=document.querySelectorAll("").forEach(function(item)){
+    item.addEventListener("click", function (e) {
+        inputValue.innerText= e.target.innerText,trim();
+    });
+}
+
